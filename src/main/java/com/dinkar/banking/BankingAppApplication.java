3df21f3bf4ca225dd0d69c96cdc1fbde1b30536b
dinkarkr <dinkar6322@gmail.com>
2024-03-23 14:00:12 +0530

    removing unnecessary autowired object

--- src/main/java/com/dinkar/banking/BankingAppApplication.java
+++ src/main/java/com/dinkar/banking/BankingAppApplication.java
@@ -1,16 +1,10 @@
 package com.dinkar.banking;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.dinkar.banking.repository.AccountRepository;
-
 @SpringBootApplication
 public class BankingAppApplication {
-	
-	@Autowired
-	AccountRepository repository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(BankingAppApplication.class, args);
