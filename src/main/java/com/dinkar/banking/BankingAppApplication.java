f7325b734a2c0b1962afdff6503fedda861014a8
dinkarkr <dinkar6322@gmail.com>
2024-03-23 13:39:20 +0530

    removing sysout

--- src/main/java/com/dinkar/banking/BankingAppApplication.java
+++ src/main/java/com/dinkar/banking/BankingAppApplication.java
@@ -3,8 +3,6 @@ package com.dinkar.banking;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.event.ContextRefreshedEvent;
-import org.springframework.context.event.EventListener;
 
 import com.dinkar.banking.repository.AccountRepository;
 
@@ -16,11 +14,6 @@ public class BankingAppApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BankingAppApplication.class, args);
-	}
-
-	@EventListener()
-	public void findById(ContextRefreshedEvent contextRefreshedEvent) {
-		System.out.println(repository.findAll()); 
 	}
 	
 }
